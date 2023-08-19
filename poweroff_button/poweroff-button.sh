8738dbd630d1a427397fbb7634a93cef62ab9b67
Eugene Stahov <eugene.stahov@gmail.com>
2023-08-20 01:20:04 +0500

    pure bash working example

--- poweroff_button/poweroff-button.sh
+++ poweroff_button/poweroff-button.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-#THIS IS PLACEHOLDER, WORK IN PROGRESS
+#CB1 - example PG9 (32) -> GND (30)
+gpiochip=gpiochip0
+pin=201
 
-safe_temp=50
+safe_temp=25
+
+#avoid exit if no physical button is connected
+if [ `sudo gpioget -B pull-up $gpiochip $pin` -eq 1 ]; then
+        >&2 echo "Button should not be pressed so early, potential wiring problem"
+        exit 1
+fi;
+
 
 while :
 do
         #button check placeholder
-        sleep 1
+        sleep 0.2
+        [ `sudo gpioget -B pull-up $gpiochip $pin` -eq 0 ] && continue
+
+        echo "button event"
 
         # ready to poweroff in klipper shutdown state
         curl -s http://127.0.0.1:7125/printer/objects/query?webhooks | jq -M .result.status.webhooks.state | grep -q shutdown && break
@@ -22,4 +34,4 @@ do
         [ $is_safe -eq 1 ] && break
 done
 
-#sudo poweroff
+sudo poweroff
